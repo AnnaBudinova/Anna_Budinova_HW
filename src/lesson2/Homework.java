9a23b4c91a53951d28b8108706a911f690575730
AnnaBudinova <anbori@abv.bg>
2023-06-23 14:12:09 +0300

    Add HW for lesson 2

--- src/lesson2/Homework.java
+++ src/lesson2/Homework.java
@@ -86,6 +86,5 @@ public class Homework {
         long days = (min/minutesInHour/hourInDay) % daysInShortYear;
 
         System.out.println(min + " minutes is approximately " + years + " years and " + days + " days");
-
     }
 }
