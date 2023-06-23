c90ca90ac94cb2589c70c1d808a8ef06fed28dae
AnnaBudinova <anbori@abv.bg>
2023-06-23 14:19:16 +0300

    Add lesson 4 folder

---
+++ src/lesson4/Homework.java
@@ -0,0 +1,11 @@
+package lesson4;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Homework {
+    public static void main(String[] args) {
+        sumArray();
+    }
+    public static void sumArray() {
+
+    }
+}
