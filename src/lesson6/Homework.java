486109000debe14118dc3298e77231ea96e28bd2
AnnaBudinova <anbori@abv.bg>
2023-06-27 15:57:21 +0300

    Add lesson 6 HW

--- src/lesson6/Homework.java
+++ src/lesson6/Homework.java
@@ -1,4 +1,26 @@
 package lesson6;
+import java.util.Scanner;
 
 public class Homework {
+    public static void main(String[] args) {
+        int number = findSmallestNumber(45,4,-36);
+        System.out.println(number);
+    }
+
+  /*  static int findSmallestNumber(int num1, int num2, int num3) {
+        int smallestNumber = num1;
+
+        if (smallestNumber > num2) {
+            smallestNumber = num2;
+        }
+
+        if (smallestNumber > num3) {
+            smallestNumber = num3;
+        }
+        return smallestNumber;
+    }*/
+
+     public static int findSmallestNumber(int num1, int num2, int num3) {
+        return  (num1 + num2 + num3) / 3;
+    }
 }
