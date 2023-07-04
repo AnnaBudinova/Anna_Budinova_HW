929281c226ab181812fc698450601ed3a593b2e9
AnnaBudinova <anbori@abv.bg>
2023-07-04 16:36:49 +0300

    Add HW for lesson 6

--- src/lesson6/Homework.java
+++ src/lesson6/Homework.java
@@ -3,15 +3,10 @@ import java.util.Scanner;
 
 public class Homework {
     public static void main(String[] args) {
-        int number = averageNumber(10,5,15);
-        int number1 = findSmallestNumber(6,-5,4);
-        System.out.println(number);
-        System.out.println(number1);
-
     }
 
-      static int findSmallestNumber(int num1, int num2, int num3) {
-        int smallestNumber = num1;
+    public static double findSmallestNumber(double num1, double num2, double num3) {
+        double smallestNumber = num1;
 
         if (smallestNumber > num2) {
             smallestNumber = num2;
@@ -20,10 +15,68 @@ public class Homework {
         if (smallestNumber > num3) {
             smallestNumber = num3;
         }
+
         return smallestNumber;
     }
 
-     public static int averageNumber(int num1, int num2, int num3) {
-        return  (num1 + num2 + num3) / 3;
+    public static double computeAverageNumber(double num1, double num2, double num3) {
+        return (num1 + num2 + num3) / 3;
+    }
+
+    public static void displayMiddleChar(String value) {
+        int position;
+        int length;
+
+        if (value.length() % 2 == 0) {
+            position = value.length() / 2 - 1;
+            length = 2;
+        } else {
+            position = value.length() / 2;
+            length = 1;
+        }
+        System.out.println(value.substring(position, position + length));
+    }
+
+    public static int countWords(String text) {
+        text = text.trim();
+        int count = 0;
+        int textLength = text.length();
+
+        if (textLength > 1) {
+            count++;
+
+            for (int i = 1; i < textLength; i++) {
+                if (text.charAt(i) == ' ') {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+    public static boolean isPalindrome() {
+        int remainder;
+        int sum = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any number ");
+
+        int num = scanner.nextInt();
+
+        if (num >= 0) {
+            int palindrome = num;
+
+            while (palindrome > 0) {
+                remainder = palindrome % 10;
+                sum = (sum * 10) + remainder;
+                palindrome = palindrome / 10;
+            }
+            return num == sum;
+        } else {
+            System.out.println("The integer must be non-negative!");
+            return false;
+        }
     }
 }
